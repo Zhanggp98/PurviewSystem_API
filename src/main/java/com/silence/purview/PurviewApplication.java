8620433f9e6e45852f39b5be2559facdd8bf746a
-野原大叔 <zhanguangping98@163.com>
2022-03-12 20:46:18 +0800

    初次上传，空项目

---
+++ src/main/java/com/silence/purview/PurviewApplication.java
@@ -0,0 +1,13 @@
+package com.silence.purview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silence/purview/PurviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silence.purview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
